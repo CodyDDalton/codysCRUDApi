095779ce721a92285a689e3369568566e041cf68
unknown <cddalton@student.fullsail.edu>
2023-02-13 21:55:40 -0500

    Styling

--- reactjs/src/pages/Movie.js
+++ reactjs/src/pages/Movie.js
@@ -111,33 +111,47 @@ function Movie() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Movie Listing</h1>
-        <h5>{values && values.title}</h5>
-        <p>{values && values.genre}</p>
-        <p>{values && values.released}</p>
-        <button onClick={() => deleteMovie()}>Delete Movie</button>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/">Home</Link>
-
-        <form onSubmit={(event) => handleSubmit(event)}>
-            <label>
-                Title:
-                <input type="text" name="title" value={values.title} onChange={handleInputChange} />
-            </label>
-            <label>
-                Genre:
-                <input type="text" name="genre" value={values.genre} onChange={handleInputChange} />
-            </label>
-            <label>
-                Released:
-                <input type="text" name="released" value={values.released} onChange={handleInputChange} />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+      <header className="App-header" style={styles.header}>
+        <nav>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/">Home</Link>
+        </nav>
+        <div>
+          <h1>Movie Listing</h1>
+          <h5>{values && values.title}</h5>
+          <p>{values && values.genre}</p>
+          <p>{values && values.released}</p>
+          <button onClick={() => deleteMovie()}>Delete Movie</button>
+          
+          <form onSubmit={(event) => handleSubmit(event)}>
+              <label>
+                  Title:
+                  <input type="text" name="title" value={values.title} onChange={handleInputChange} />
+              </label>
+              <label>
+                  Genre:
+                  <input type="text" name="genre" value={values.genre} onChange={handleInputChange} />
+              </label>
+              <label>
+                  Released:
+                  <input type="text" name="released" value={values.released} onChange={handleInputChange} />
+              </label>
+              <input type="submit" value="Submit" />
+          </form>
+        </div>
       </header>
     </div>
   );
 }
 
 export default Movie;
+
+const styles = {
+  header:{
+    backgroundColor: '#f60000',
+    opacity: 1,
+    backgroundImage: 'linear-gradient(to right, #ee0000, #ee0000 11.5px, #f60000 11.5px, #f60000 )',
+    backgroundSize: '23px 100%',
+    height:'10%'
+  }
+}
