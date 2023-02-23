b80cf860acbab8f05ec6c160bbd59c387fc46d48
unknown <cddalton@student.fullsail.edu>
2023-02-23 16:08:58 -0500

    Commits

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react";
+// import { useState, useEffect } from "react";
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 
 function App() {
+    // const [currentUser, setCurrentUser] = useState(null);
+
+    // useEffect(() => {
+    //     const user = AuthService.getCurrentUser();
+    //     if(user){
+    //         setCurrentUser(user)
+    //     }
+    // }, [])
+
+
     return(
-        <Router>
-            <Routes>
-                <Route path="/movie/:id" element={<Movie />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/" exact element={<Home />} />
-            </Routes>
-        </Router>
+        <div>
+            <section>
+                <Routes>
+                    <Route path="/movie/:id" element={<Movie />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/" exact element={<Home />} />
+                </Routes>
+            </section>
+        </div>
 
     )
 }
