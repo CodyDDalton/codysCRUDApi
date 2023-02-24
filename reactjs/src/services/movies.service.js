965dea8fcdc3551e4a456529ca81510503c6e7db
unknown <cddalton@student.fullsail.edu>
2023-02-24 17:32:55 -0500

    Ready to deploy

--- reactjs/src/services/movies.service.js
+++ reactjs/src/services/movies.service.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = "http://localhost:8000/api/v1/movies";
+const API_URL = "/movies";
+const API_BASE = process.env.NODE_ENV === 'development'
+? 'http://localhost:8000/api/v1'
+: process.env.REACT_APP_BASE_URL;
 
 const getAllPrivatePosts = () => {
-    return axios.get(`${API_URL}/`, { headers: authHeader() })
+    return axios.get(`${API_BASE}${API_URL}`, { headers: authHeader() })
 }
 
 const moviesService = {
